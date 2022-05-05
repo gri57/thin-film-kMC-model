18628bdff4fdfc482a65e7cbfa5fe020d5a79552
Grigoriy Kimaev <gkimaev@uwaterloo.ca>
2022-05-05 01:09:52 -0400

    Put the KMC-related code in functions.py, added some functions for solving PDEs, and disabled Python contracts since the module is not installed on my computer (yet).

--- functions.py
+++ functions.py
@@ -2,11 +2,12 @@
 # File with the functions called by main.py
 
 
-from contracts import contract
+#from contracts import contract
 import numpy as np
+from scipy.integrate import odeint
 
 
-@contract( N='int,>0', x='int,>=0,<N', y='int,>=0,<N', surfacemat='ndarray', neighsmat='ndarray', neighstally='ndarray', returns='ndarray,ndarray,ndarray' )
+#@contract( N='int,>0', x='int,>=0,<N', y='int,>=0,<N', surfacemat='ndarray', neighsmat='ndarray', neighstally='ndarray', returns='ndarray,ndarray,ndarray' )
 def adsorption_event( N, x, y, surfacemat, neighsmat, neighstally ):
 	
 	""" 
@@ -23,7 +24,7 @@ def adsorption_event( N, x, y, surfacemat, neighsmat, neighstally ):
 	return surfacemat, neighsmat, neighstally
 
 
-@contract( N='int,>0', x='int,>=0,<N', y='int,>=0,<N', surfacemat='ndarray', neighsmat='ndarray', neighstally='ndarray', returns='ndarray,ndarray,ndarray' )
+#@contract( N='int,>0', x='int,>=0,<N', y='int,>=0,<N', surfacemat='ndarray', neighsmat='ndarray', neighstally='ndarray', returns='ndarray,ndarray,ndarray' )
 def desorption_event( N, x, y, surfacemat, neighsmat, neighstally ):
 	
 	""" 
@@ -40,7 +41,7 @@ def desorption_event( N, x, y, surfacemat, neighsmat, neighstally ):
 	return surfacemat, neighsmat, neighstally
 
 
-@contract( N='int,>0', xi='int,>=0,<N', yi='int,>=0,<N', xf='int,>=0,<N', yf='int,>=0,<N', surfacemat='ndarray', neighsmat='ndarray', neighstally='ndarray', returns='ndarray,ndarray,ndarray' )
+#@contract( N='int,>0', xi='int,>=0,<N', yi='int,>=0,<N', xf='int,>=0,<N', yf='int,>=0,<N', surfacemat='ndarray', neighsmat='ndarray', neighstally='ndarray', returns='ndarray,ndarray,ndarray' )
 def migration_event( N, xi, yi, xf, yf, surfacemat, neighsmat, neighstally ):
 	
 	"""
@@ -58,7 +59,7 @@ def migration_event( N, xi, yi, xf, yf, surfacemat, neighsmat, neighstally ):
 	return surfacemat, neighsmat, neighstally
 
 
-@contract( N='int,>0', callerid='str', x='int,>=0,<N', y='int,>=0,<N', surfacemat='ndarray', neighsmat='ndarray', neighstally='ndarray', returns='ndarray,ndarray' )
+#@contract( N='int,>0', callerid='str', x='int,>=0,<N', y='int,>=0,<N', surfacemat='ndarray', neighsmat='ndarray', neighstally='ndarray', returns='ndarray,ndarray' )
 def update_neighs_pbc( N, callerid, x, y, surfacemat, neighsmat, neighstally ):
 	
 	""" 
@@ -133,7 +134,7 @@ def update_neighs_pbc( N, callerid, x, y, surfacemat, neighsmat, neighstally ):
 	return neighsmat, neighstally
 
 
-@contract( neighstally='ndarray', f='int,>=1,<=5', returns='float' )
+#@contract( neighstally='ndarray', f='int,>=1,<=5', returns='float' )
 def calcWm( neighstally, f = 5 ):
 	
 	"""
@@ -160,7 +161,7 @@ def calcWm( neighstally, f = 5 ):
 	return Wm
 
 
-@contract( neighstally='ndarray', returns='float' )
+#@contract( neighstally='ndarray', returns='float' )
 def calcWd( neighstally ):
 	
 	"""
@@ -180,7 +181,7 @@ def calcWd( neighstally ):
 	return Wd
 
 
-@contract( n='int,>=1,<=5', returns='float' )
+#@contract( n='int,>=1,<=5', returns='float' )
 def calcPd( n ):
 	
 	"""
@@ -196,7 +197,7 @@ def calcPd( n ):
 	return Pd
 
 
-@contract( n='int,>=1,<=5', returns='float' )
+#@contract( n='int,>=1,<=5', returns='float' )
 def calcPm( n ):
 	
 	"""
@@ -210,4 +211,155 @@ def calcPm( n ):
 	Pm = A * calcPd( n )
 	
 	return Pm
+
+
+#@contract( N='int,>0', surfacemat='ndarray', neighsmat='ndarray', neighstally='ndarray', Wa='float', Wd='float', Wm='float', dtkmc='float', returns='float' )
+def runSolidOnSolidKMC( N, surfacemat, neighsmat, neighstally, dtkmc, Wa, Wd, Wm ):
+	
+	"""
+	Microscale model - solid-on-solid with adsorption, migration and desorption.
+	"""
+	
+	# Use Wa, Wd and Wm to choose between adsorption, desorption or migration (a/d/m)
+	Wtotal_inv = np.power( Wa+Wd+Wm, -1. )
+
+	# Get a random number on the half-open interval [1e-53,1.0) to decide which event to perform
+	# 1e-53 is practically zero
+	zeta = np.random.uniform( low=1e-53, high=1.0 )
+
+	# In an if/elif/else structure, as soon as one of the conditions is true, the code under the 
+	# condition is executed and all other conditions are skipped.
+
+	if zeta < Wa*Wtotal_inv:
+		# perform adsorption
+		# choose a site randomly (pick a random index for the arrays, Python indexing starts at zero)
+		x = np.random.random_integers(0,N-1)
+		y = np.random.random_integers(0,N-1)
+		surfacemat, neighsmat, neighstally = adsorption_event( N, x, y, surfacemat, neighsmat, neighstally )
+		
+	elif zeta < (Wa+Wd)*Wtotal_inv:
+		# perform desorption
+		# choose a site randomly (pick a random index for the arrays, Python indexing starts at zero)
+		x = np.random.random_integers(0,N-1)
+		y = np.random.random_integers(0,N-1)
+		surfacemat, neighsmat, neighstally = desorption_event( N, x, y, surfacemat, neighsmat, neighstally )
+		
+	else:
+		# perform migration
+		# choose one of the 5 "classes" (atoms with 1 neighbour, 2, 3, 4 or 5)
+		
+		# it is already known that the value of zeta is between (Wa+Wd)/(Wa+Wd+Wm) and 1
+		# Wm is calculated by summing Mi*Pm(i) products for i from 1 to 5 (as per equation 3-15 of Shabnam's PhD thesis)
+		# zeta can be anywhere between (Wa+Wd)/(Wa+Wd+Wm(i=5)) and (Wa+Wd+Wm(i))/(Wa+Wd+Wm(i=5)), where 1<=i<=5 ("i" is an integer)
+		
+		# modify zeta to save on CPU time (less calculations); the expression below is: zeta*(Wa+Wd+Wm)-Wa-Wd
+		zetamod = zeta * np.power( Wtotal_inv, -1. ) - Wa - Wd
+		
+		# find "i" value that matches to where zeta is on this discretized region
+		Wm_vari = 0.0
+		neighclass = 0 
+		while zetamod > Wm_vari:
+			neighclass += 1
+			Wm_vari += calcPm( neighclass )
+			# this "while" loop will stop as soon as zetamod <= Wm_vari, leaving us with the correct "neighclass" value
+		del Wm_vari # housekeeping
+		
+		# find out the x and y indeces of atoms that have the specified number of neighbours
+		indecesofatoms = np.where( neighsmat == neighclass )
+		
+		# find out how many atoms have the specified number of neighbours
+		numofatoms = len( indecesofatoms[0] )
+		
+		# choose one of those atoms at random
+		chosenatom = np.random.random_integers( 0, numofatoms-1 )
+		
+		# get the atom's x and y coordinates
+		xi = indecesofatoms[0][chosenatom]
+		yi = indecesofatoms[1][chosenatom]
+		
+		# randomly find the location xf,yf where the atom selected above will migrate to by adding/subtracting 1 to xi/yi
+		chosenneighbour = np.random.random_integers( 0, 3 )
+		if chosenneighbour == 0:
+			xf = xi - 1
+			yf = yi
+			# use periodic boundary conditions
+			if xf < 0:
+				xf += N
+		elif chosenneighbour == 1:
+			xf = xi + 1
+			yf = yi
+			# use periodic boundary conditions
+			if xf >= N:
+				xf -= N
+		elif chosenneighbour == 2:
+			xf = xi
+			yf = yi - 1
+			# use periodic boundary conditions
+			if yf < 0:
+				yf += N
+		elif chosenneighbour == 3:
+			xf = xi
+			yf = yi + 1
+			# use periodic boundary conditions
+			if yf >= N:
+				yf -= N
+		
+		# Perform the migration event
+		surfacemat, neighsmat, neighstally = migration_event( N, xi, yi, xf, yf, surfacemat, neighsmat, neighstally )
+
+
+	# Increment the KMC timestep (equation 3-16 of Shabnam's PhD thesis)
+	# 1e-53 is practically zero, but prevents np.log(sigma) from giving "-inf" for an answer
+	sigma = np.random.uniform( low=1e-53, high=1.0 )
+	dtkmc += -np.log( sigma ) * Wtotal_inv    # equation 3-16
+	
+	return dtkmc
+
+
+#@contract()
+def calcFluidFlowSS():
+	
+	"""
+	Solve the fluid flow conservation equation at steady state.
+	"""
+	
+	# Parameter values
+	rho_b = 1.0
+	rho = 1.0
+	params = [rho_b, rho] # pack the parameter values for the ODE solver
+	
+	# Initial values of the dependent variable and its derivatives
+	f0 = 0.0
+	f_eta_0 = 0.0
+	f_eta_2_0 = 0.0
+	fvars = [ f0, f_eta_0, f_eta_2_0 ] # pack the dependent variable values for the ODE solver
+	
+	# Make an array for the independent variable
+	etaStop = 1.0
+	etaInc = 0.01
+	eta = np.arange( 0., etaStop, etaInc )
+	
+	# Use the ode solver imported from scipy.integrate
+	fvalues = odeint( FluidFlowSS, fvars, eta, args=(params,)  )
+	
+	return fvalues
+
+
+#@contract( fvars='list', eta='ndarray', params='list', returns='list' )
+def FluidFlowSS( fvars, eta, params ):
+	
+	"""
+	The fluid flow conservation equation at steady state - equation 3-1 
+	of Shabnam's PhD thesis at steady state.
+	"""
+	
+	f, f_eta, f_eta_2 = fvars # unpack the dependent variable, and its first and second derivatives with respect to eta
+	
+	rho_b, rho = params # unpack the parameter values
+	
+	derivs = [ f_eta,
+			   f_eta_2,
+			   -f * f_eta_2 - 0.5 * ( rho_b/rho - f_eta**2. ) ]
+	
+	return derivs
 
--- main.py
+++ main.py
@@ -1,127 +1,66 @@
 # 2015-Nov-16
 # Script for the main program that will drive multiscale simulation
 
+
 from functions import *
 import numpy as np
 
 
 # Number of sites along an edge of the square simple cubic lattice
-# N^2 is the total number of atoms
+# 	N^2 is the total number of atoms
 N = 20
 
+# Array with the number of atoms at every site
 surfacemat = np.ndarray( (N,N), 'int' )
+
+# Array with the number of neighbours of each atom at every site
 neighsmat = np.ndarray( (N,N), 'int' )
+
+# Array that tells how many atoms in total there are with 1, 2, 3, 4 and 5 neighbours
 neighstally = np.ndarray( 5, 'int' )
+
+# Coupling time for the PDE gas phase and KMC solid-on-solid models
+dtcouple = 0.1
+
+# KMC timestep
+dtkmc = 0.0
+
+# Total simulation time
+Ttot = 1.0
+
+# Current simulation time
+Tcurr = 0.0
+
+
+while Tcurr < Ttot:
+
+	# Calculate Wa, Wd, Wm
+	Wa = Pa * np.power( N, 2. )
+	Wd = calcWd( neighstally )
+	Wm = calcWm( neighstally )
+
+	# Check the time, integrate PDE equations if coupling time has been reached
+	if dtkmc < dtcouple:
+		
+		### Microscale model (solid-on-solid) ###
+		# update of dtkmc is done within the function
+		dtkmc = runSolidOnSolidKMC( N, surfacemat, neighsmat, neighstally, Wa, Wd, Wm, dtkmc )
+		
+	else:
+		
+		# update the total simulation time
+		Tcurr += dtkmc
+		
+		# run the PDE model to get the new boundary condition value and update xgrow for the KMC model
+		runGasPhasePDE()
+
 
 
 ### Macroscale model (PDE model) ###
 
-# Integrate the conservation equations
+
+# Integrate the gas phase conservation equations
+
 
 # Calculate boundary conditions
-
-
-### Microscale model (solid-on-solid) ###
-
-# Variables - N of lattice sites, KMC time, coupling time
-
-# Calculate Wa, Wd, Wm
-Wa = Pa * np.power( N, 2. )
-Wd = calcWd( neighstally )
-Wm = calcWm( neighstally )
-
-# Use Wa, Wd and Wm to choose between adsorption, desorption or migration (a/d/m)
-Wtotal_inv = np.power( Wa+Wd+Wm, -1. )
-
-# Get a random number on the half-open interval [1e-53,1.0) to decide which event to perform
-# 1e-53 is practically zero
-zeta = np.random.uniform( low=1e-53, high=1.0 )
-
-# In an if/elif/else structure, as soon as one of the conditions is true, everything below is skipped.
-
-if zeta < Wa*Wtotal_inv:
-	# perform adsorption
-	# choose a site randomly (pick a random index for the arrays, Python indexing starts at zero)
-	x = np.random.random_integers(0,N-1)
-	y = np.random.random_integers(0,N-1)
-	surfacemat, neighsmat, neighstally = adsorption_event( N, x, y, surfacemat, neighsmat, neighstally )
-	
-elif zeta < (Wa+Wd)*Wtotal_inv:
-	# perform desorption
-	# choose a site randomly (pick a random index for the arrays, Python indexing starts at zero)
-	x = np.random.random_integers(0,N-1)
-	y = np.random.random_integers(0,N-1)
-	surfacemat, neighsmat, neighstally = desorption_event( N, x, y, surfacemat, neighsmat, neighstally )
-	
-else:
-	# perform migration
-	# choose one of the 5 "classes" (atoms with 1 neighbour, 2, 3, 4 or 5)
-	
-	# it is already known that the value of zeta is between (Wa+Wd)/(Wa+Wd+Wm) and 1
-	# Wm is calculated by summing Mi*Pm(i) products for i from 1 to 5 (as per equation 3-15 of Shabnam's PhD thesis)
-	# zeta can be anywhere between (Wa+Wd)/(Wa+Wd+Wm(i=5)) and (Wa+Wd+Wm(i))/(Wa+Wd+Wm(i=5)), where 1<=i<=5 ("i" is an integer)
-	
-	# modify zeta to save on CPU time (less calculations); the expression below is: zeta*(Wa+Wd+Wm)-Wa-Wd
-	zetamod = zeta * np.power( Wtotal_inv, -1. ) - Wa - Wd
-	
-	# find "i" value that matches to where zeta is on this discretized region
-	Wm_vari = 0.0
-	neighclass = 0 
-	while zetamod > Wm_vari:
-		neighclass += 1
-		Wm_vari += calcPm( neighclass )
-		# this "while" loop will stop as soon as zetamod <= Wm_vari, leaving us with the correct "neighclass" value
-	del Wm_vari # housekeeping
-	
-	# find out the x and y indeces of atoms that have the specified number of neighbours
-	indecesofatoms = np.where( neighsmat == neighclass )
-	
-	# find out how many atoms have the specified number of neighbours
-	numofatoms = len( indecesofatoms[0] )
-	
-	# choose one of those atoms at random
-	chosenatom = np.random.random_integers( 0, numofatoms-1 )
-	
-	# get the atom's x and y coordinates
-	xi = indecesofatoms[0][chosenatom]
-	yi = indecesofatoms[1][chosenatom]
-	
-	# randomly find the location xf,yf where the atom selected above will migrate to by adding/subtracting 1 to xi/yi
-	chosenneighbour = np.random.random_integers( 0, 3 )
-	if chosenneighbour == 0:
-		xf = xi - 1
-		yf = yi
-		# use periodic boundary conditions
-		if xf < 0:
-			xf += N
-	elif chosenneighbour == 1:
-		xf = xi + 1
-		yf = yi
-		# use periodic boundary conditions
-		if xf >= N:
-			xf -= N
-	elif chosenneighbour == 2:
-		xf = xi
-		yf = yi - 1
-		# use periodic boundary conditions
-		if yf < 0:
-			yf += N
-	elif chosenneighbour == 3:
-		xf = xi
-		yf = yi + 1
-		# use periodic boundary conditions
-		if yf >= N:
-			yf -= N
-	
-	# Perform the migration event
-	surfacemat, neighsmat, neighstally = migration_event( N, xi, yi, xf, yf, surfacemat, neighsmat, neighstally )
-
-
-# Increment the KMC timestep
-# 1e-53 is practically zero, but prevents np.log(sigma) from giving "-inf" for an answer
-sigma = np.random.uniform( low=1e-53, high=1.0 )
-dtkmc += -np.log( sigma ) * Wtotal_inv
-
-# Check the time, integrate PDE equations if coupling time has been reached
-dtkmc < dtcouple
 
